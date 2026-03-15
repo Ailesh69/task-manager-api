ad9ef276b5deab0ee680e9745992340823d8cf1b
Ailesh69 <aileshsharma354@gmail.com>
2026-03-16 01:27:18 +0530

    feat: add rate limiting with slowapi

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
 from typing import Annotated , Optional
 from fastapi import FastAPI , Depends , HTTPException
-from . import models , schemas , crud 
+from starlette.requests import Request
+
+from . import models , schemas , crud
 from .db import engine , open_db
 from sqlalchemy.orm import Session
 from .auth import verify_pass , create_access_token , get_current_user
+from slowapi import Limiter , _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+from fastapi.responses import JSONResponse
 
-
+limiter =Limiter(get_remote_address)
 app = FastAPI(title="Task manager")
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded,_rate_limit_exceeded_handler)
 
 # Reusable dependency types
 db_dep = Annotated[Session, Depends(open_db)]
@@ -14,7 +22,8 @@ user_dep = Annotated[dict, Depends(get_current_user)]
 
 # --- USER ENDPOINTS ---
 @app.post("/users/", response_model=schemas.UserResponse, tags=["Users"])
-def create_user(user : schemas.CreateUser , db : db_dep):
+@limiter.limit("10/minute")
+def create_user(request : Request , user : schemas.CreateUser , db : db_dep):
     if crud.get_user_by_email(db, email=user.email):
         raise HTTPException(status_code=400, detail="Email already registered")
     if crud.get_user_by_contact(db, contact_num=user.contact_num):
@@ -22,20 +31,24 @@ def create_user(user : schemas.CreateUser , db : db_dep):
     return crud.create_user(db , user)
 
 @app.get("/users/", response_model=list[schemas.UserResponse], tags=["Users"])
-def get_all_users(db: db_dep, current_user: user_dep):
+@limiter.limit("10/minute")
+def get_all_users(request : Request ,db: db_dep, current_user: user_dep):
     return crud.get_user(db)
 
 # --- TASK ENDPOINTS (Protected) ---
 
 # STEP 1: Update Route
 @app.get("/tasks/me", response_model=list[schemas.TaskResponse], tags=["Tasks"])
-def get_my_tasks(db: db_dep , current_user : user_dep, skip : int = 0 , limit : int = 10 , completed : Optional[bool] = None , sort : str = "created_at" , order : str = "desc"):
+@limiter.limit("10/minute")
+def get_my_tasks(request : Request , db: db_dep , current_user : user_dep, skip : int = 0 , limit : int = 10 , completed : Optional[bool] = None , sort : str = "created_at" , order : str = "desc"):
     user_id = current_user["user_id"]
     return crud.get_task_by_user(db , user_id, skip, limit, completed)
 
 # STEP 2: Update Create Task
 @app.post("/tasks", response_model=schemas.TaskResponse, tags=["Tasks"])
+@limiter.limit("10/minute")
 def create_task(
+    request : Request ,
     task : schemas.CreateTask , 
     db: db_dep, 
     current_user: user_dep
@@ -46,7 +59,9 @@ def create_task(
 
 # STEP 3: Remove user_id from update/delete if possible
 @app.put("/tasks/{task_id}", response_model=schemas.TaskResponse, tags=["Tasks"])
+@limiter.limit("10/minute")
 def update_task_status(
+    request : Request ,
     task_id:int , 
     completed:bool , 
     db: db_dep, 
@@ -59,7 +74,8 @@ def update_task_status(
     return task 
 
 @app.delete("/task/{task_id}", tags=["Tasks"])
-def delete_task_by_id(task_id:int , db: db_dep, current_user: user_dep):
+@limiter.limit("10/minute")
+def delete_task_by_id(request : Request ,task_id:int , db: db_dep, current_user: user_dep):
     user_id = current_user["user_id"]
     task = crud.delete_task(db , task_id, user_id)
     if not task : 
@@ -68,7 +84,8 @@ def delete_task_by_id(task_id:int , db: db_dep, current_user: user_dep):
 
 # --- AUTHENTICATION ---
 @app.post("/login", response_model=schemas.Token, tags=["Authentication"])
-def login(email:str , password : str ,db : db_dep):
+@limiter.limit("5/minute")
+def login(request : Request ,email:str , password : str ,db : db_dep):
     user = crud.get_user_by_email(db, email)
     if not user or not verify_pass(password, user.password): 
         raise HTTPException(status_code=401 , detail="Invalid email or password")
